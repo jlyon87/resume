39c8dbaa69b7452fa59b5ed76e7476e87f274760
jlyonas <jlyon@acumensolutions.com>
2017-03-29 07:59:29 -0400

    remove express routes for static resources

--- app/router.js
+++ app/router.js
@@ -9,29 +9,29 @@ router.use(function timeLog (req, res, next) {
 	next();
 });
 
-const cssRouter = require('./routes/css-router.js');
-const jsRouter = require('./routes/js-router.js');
-const moduleRouter = require('./routes/module-router.js');
+// const cssRouter = require('./routes/css-router.js');
+// const jsRouter = require('./routes/js-router.js');
+// const moduleRouter = require('./routes/module-router.js');
 
-router.use('/css', cssRouter);
-router.use('/js', jsRouter);
-router.use('/modules', moduleRouter);
+// router.use('/css', cssRouter);
+// router.use('/js', jsRouter);
+// router.use('/modules', moduleRouter);
 
 router.get('/', function(req, res) {
 	res.sendFile( path.join(__dirname, 'index.html') );
 });
 
-router.get('/home', function(req, res) {
-	res.sendFile( path.join(__dirname, 'index.html') );
-});
-
-router.get('/app.module.js', function(req, res) {
-	res.sendFile( path.join(__dirname, 'app.module.js') );
-});
-
-router.get('/app.config.js', function(req, res) {
-	res.sendFile( path.join(__dirname, 'app.config.js') );
-});
+// router.get('/home', function(req, res) {
+// 	res.sendFile( path.join(__dirname, 'index.html') );
+// });
+//
+// router.get('/app.module.js', function(req, res) {
+// 	res.sendFile( path.join(__dirname, 'app.module.js') );
+// });
+//
+// router.get('/app.config.js', function(req, res) {
+// 	res.sendFile( path.join(__dirname, 'app.config.js') );
+// });
 
 router.get('/favicon5.ico', function(req, res) {
 	res.sendFile( path.join(__dirname, 'assets', 'favicon5.ico') );
